c85cce1c7f2dcd738c4fd8ed0499bb73d0404d56
Mathias Rieder <mathias.rieder@gmail.com>
2025-05-15 06:25:53 +0000

    implementation

--- ringbuffer.py
+++ ringbuffer.py
@@ -1,17 +1,31 @@
-
 class Ringbuffer:
     def __init__(self, capacity):
-        pass
-        
+        self.capacity = capacity
+        self.buffer = [None] * capacity
+        self.start = 0  # Points to the oldest element
+        self.end = 0    # Points to the next write position
+        self.size = 0
+
     # Returns the number of elements stored in the ring.
     def get_number_of_elements(self):
-        return 0
+        return self.size
 
     # Add an element to the ringbuffer. If the buffer is full, the oldest element is overwritten.
     def add(self, element):
-        self.get_number_of_elements()
-        pass
+        self.buffer[self.end] = element
+        if self.size == self.capacity:
+            # Buffer is full, move start forward (overwrite oldest)
+            self.start = (self.start + 1) % self.capacity
+        else:
+            self.size += 1
+        self.end = (self.end + 1) % self.capacity
 
     # Returns the oldest element in the ringbuffer and removes it from the buffer.
     def remove(self):
-        pass
+        if self.size == 0:
+            return None
+        element = self.buffer[self.start]
+        self.buffer[self.start] = None
+        self.start = (self.start + 1) % self.capacity
+        self.size -= 1
+        return element
--- ringbuffer_test.py
+++ ringbuffer_test.py
@@ -1,6 +1,49 @@
 from ringbuffer import Ringbuffer
 
 def test_create_ringbuffer():
+    # GIVEN an empty ringbuffer
     rb = Ringbuffer(10)
+    # THEN the buffer should have 0 elements
     assert rb.get_number_of_elements() == 0
 
+def test_add_one_element():
+    # GIVEN an empty ringbuffer
+    rb = Ringbuffer(5)
+    # WHEN I add an element
+    rb.add("a")
+    # THEN the buffer should have 1 element
+    assert rb.get_number_of_elements() == 1
+
+def test_add_and_remove_element():
+    # GIVEN an empty ringbuffer
+    rb = Ringbuffer(5)
+    rb.add("x")
+    # WHEN I remove an element
+    removed = rb.remove()
+    # THEN the removed element should be the one added and buffer should be empty
+    assert removed == "x"
+    assert rb.get_number_of_elements() == 0
+
+def test_overwrite_when_full():
+    # GIVEN a full ringbuffer
+    rb = Ringbuffer(2)
+    rb.add("first")
+    rb.add("second")
+    # WHEN I add another element
+    rb.add("third")
+    # THEN the oldest element ("first") should be overwritten
+    assert rb.get_number_of_elements() == 2
+    assert rb.remove() == "second"
+    assert rb.remove() == "third"
+
+def test_fifo_order():
+    # GIVEN a ringbuffer
+    rb = Ringbuffer(3)
+    rb.add(1)
+    rb.add(2)
+    rb.add(3)
+    # WHEN I remove elements
+    # THEN they should come out in FIFO order
+    assert rb.remove() == 1
+    assert rb.remove() == 2
+    assert rb.remove() == 3
